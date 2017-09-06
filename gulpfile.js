f0a77f3fef1a49fe76cab138f217a92fbbb84cfc
Ashik P <ashikvpapanna@gmail.com>
2017-09-06 18:54:30 +0530

    added nav bar

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,52 @@ var typescript=require('gulp-typescript');
 var del=require('del');
 var tsconfig=require('./client/tsconfig.json');
 var sourcemaps=require('gulp-sourcemaps');
+var browsersync=require('browser-sync').create();
+var nodemon=require('gulp-nodemon');
+
+
+gulp.task('nodemon',function(cb)
+{
+var started=false;
+return nodemon({
+  script:'server.js'
+
+}).on('start',function(){
+  if(!started)
+    {
+      cb(),
+      started=true;
+    }
+}).on('restart',function()
+{
+  console.log('restarted');
+}).on('crash',function()
+{
+  console.error('node crashed');
+})
+});
+gulp.task('browser-sync',['nodemon'],function()
+{
+  browsersync.init(
+    {
+      
+      proxy:{
+        target:'http://localhost:3000',
+        ws:true
+        
+      }
+    }
+  )
+});
 
 gulp.task('clean',function(){
     return del('./client/dist/**/*');
 });
 
-gulp.task('copy-html',function(){
+gulp.task('copy-html',['clean'],function(){
   return gulp.src('./client/app/**/*.html')
-  .pipe(gulp.dest('./client/dist/app'))
+  .pipe(gulp.dest('./client/dist/app'));
+  
 });
 gulp.task('compile-main',function(){
   return gulp.src('./client/main.ts')
@@ -18,15 +56,25 @@ gulp.task('compile-main',function(){
   .pipe(typescript(tsconfig.compilerOptions))
   .pipe(sourcemaps.write('.'))
   .pipe(gulp.dest('./client/dist/'));
+  
 })
 
 
-gulp.task('compile',['clean','compile-main','copy-html'],function(){
+gulp.task('compile',['copy-html','compile-main'],function(){
   return gulp.src('./client/app/**/*.ts')
   .pipe(sourcemaps.init())
   .pipe(typescript(tsconfig.compilerOptions))
   .pipe(sourcemaps.write('.'))
-  .pipe(gulp.dest('./client/dist/app'));
+  .pipe(gulp.dest('./client/dist/app')).pipe(browsersync.reload({stream:true}));
+
+
+});
+
+gulp.task('watch',['build','nodemon'],function()
+{
+  gulp.watch('./client/app/**/*.html',['build']);
+  gulp.watch('./client/app/**/*.ts',['build']);
+  gulp.watch('./client/main.ts',['build']); 
 
 });
 
