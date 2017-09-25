5217bcf5f1907886fecfb849174e586dc114e3ba
Ashik P <ashikvpapanna@gmail.com>
2017-09-25 20:18:12 +0530

    remove access key

--- server/helpers/sendgridIntegration.js
+++ server/helpers/sendgridIntegration.js
@@ -1,11 +1,12 @@
 var helper = require('sendgrid').mail;
+var appconfig=require('../../appconfig');
 var fromEmail = new helper.Email('ashikvpapanna@gmail.com');
-var toEmail = new helper.Email('ashikvp1993@gmail.com');
+var toEmail = new helper.Email('ashikvp1993@gail.com');
 var subject = 'Sending with SendGrid is Fun';
 var content = new helper.Content('text/html', '<h1>and easy to do anywhere, even with Node.js</h1>');
 var mail = new helper.Mail(fromEmail, subject, toEmail, content);
  
-var sg = require('sendgrid')('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');
+var sg = require('sendgrid')(appconfig.sendgridkey);
 var request = sg.emptyRequest({
   method: 'POST',
   path: '/v3/mail/send',
@@ -15,8 +16,10 @@ var request = sg.emptyRequest({
 sg.API(request, function (error, response) {
   if (error) {
     console.log('Error response received');
+    console.log(error);
   }
-  console.log(response.statusCode);
-  console.log(response.body);
-  console.log(response.headers);
+  console.log(process.env.NODE_ENV);
+  //console.log(response.statusCode);
+ // console.log(response.body);
+ // console.log(response.headers);
 });
