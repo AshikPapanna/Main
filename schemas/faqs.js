495e79a3196d47874765a6f298eb24bf19501c21
Ashik P <ashikvpapanna@gmail.com>
2017-10-13 19:08:38 +0530

    sdsd

---
+++ schemas/faqs.js
@@ -0,0 +1,25 @@
+
+var mongoose = require("mongoose");
+
+var Faqs = new mongoose.Schema({
+    question: {
+        type: String,
+        required: true,
+        trim: true
+       // validate: [validators.isLength({message:'FirstName should be >=4 and <=10'},4, 10)]
+    },
+    answer: {
+        type: String,
+        trim: true,
+        required:true
+       // validate: [validators.isLength({message:'FirstName should be >=2 and <=6'},4, 10)]
+    },
+    comments: [String],
+   
+   
+});
+
+
+ mongoose.model("Faqs",Faqs);
+ module.exports=mongoose.model('Faqs');
+//# sourceMappingURL=user.js.map
