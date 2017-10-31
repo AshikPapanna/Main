d4a720b765be902e3d833d496b81ae4b22eaeba9
Ashik Papanna <Ashikvpapanna@gmail.com>
2017-10-31 08:23:19 +0530

    Asa

--- public/systemjs.config.js
+++ public/systemjs.config.js
@@ -1,49 +1,65 @@
-/**
- * System configuration for Angular 2/4 samples
- * Adjust as necessary for your application needs.
- */
-(function (global) {
-    System.config({
-        paths: {
-            // paths serve as alias
-            'npm:': 'node_modules/'
-        },
-        main:'main.js',
-        // map tells the System loader where to look for things
-        map: {
-            // our app is within the app folder
-            app: 'dist',
-             'rxjs': 'npm:rxjs',
-            'angular2-in-memory-web-api':'npm:angular2-in-memory-web-api',
+(function(global) {
+    // map tells the System loader where to look for things
+    main:'main.js'
+    var map = {
+      'app':                        'dist', // 'dist',  
+      '@angular/cdk': 'node_modules/@angular/cdk/bundles/cdk.umd.js',
+      '@angular/cdk/a11y': 'node_modules/@angular/cdk/bundles/cdk-a11y.umd.js',
+      '@angular/cdk/bidi': 'node_modules/@angular/cdk/bundles/cdk-bidi.umd.js',
+      '@angular/cdk/coercion': 'node_modules/@angular/cdk/bundles/cdk-coercion.umd.js',
+      '@angular/cdk/collections': 'node_modules/@angular/cdk/bundles/cdk-collections.umd.js',
+      '@angular/cdk/keycodes': 'node_modules/@angular/cdk/bundles/cdk-keycodes.umd.js',
+      '@angular/cdk/observers': 'node_modules/@angular/cdk/bundles/cdk-observers.umd.js',
+      '@angular/cdk/overlay': 'node_modules/@angular/cdk/bundles/cdk-overlay.umd.js',
+      '@angular/cdk/platform': 'node_modules/@angular/cdk/bundles/cdk-platform.umd.js',
+      '@angular/cdk/portal': 'node_modules/@angular/cdk/bundles/cdk-portal.umd.js',
+      '@angular/cdk/rxjs': 'node_modules/@angular/cdk/bundles/cdk-rxjs.umd.js',
+      '@angular/cdk/table': 'node_modules/@angular/cdk/bundles/cdk-table.umd.js',
+      '@angular/cdk/scrolling': 'node_modules/@angular/cdk/bundles/cdk-scrolling.umd.js',
+      '@angular/cdk/stepper': 'node_modules/@angular/cdk/bundles/cdk-stepper.umd.js',
 
-            // angular bundles
-            '@angular/core': 'npm:@angular/core/bundles/core.umd.js',
-            '@angular/common': 'npm:@angular/common/bundles/common.umd.js',
-            '@angular/compiler': 'npm:@angular/compiler/bundles/compiler.umd.js',
-            '@angular/platform-browser': 'npm:@angular/platform-browser/bundles/platform-browser.umd.js',
-            '@angular/platform-browser-dynamic': 'npm:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
-            '@angular/http': 'npm:@angular/http/bundles/http.umd.js',
-            '@angular/router': 'npm:@angular/router/bundles/router.umd.js',
-            '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
-            '@angular/animations': 'npm:@angular/animations/bundles/animations.umd.js',
-            '@angular/animations/browser': 'npm:@angular/animations/bundles/animations-browser.umd.js',
-            '@angular/platform-browser/animations': 'npm:@angular/platform-browser/bundles/platform-browser-animations.umd.js'
 
-            // other libraries
-           
-        },
-        
-        // packages tells the System loader how to load when no filename and/or no extension
-        packages: {
-            app: {
-                main: 'main.js',
-                defaultExtension: 'js'
-            },
-            rxjs: {
-                main:'Rx.js',
-                defaultExtension: 'js'
-            },
-              'angular2-in-memory-web-api': { main: 'index.js', defaultExtension: 'js' },
-        }
-    });
-})(this);
+      '@angular':                   'node_modules/@angular',
+      
+      'angular2-in-memory-web-api': 'node_modules/angular2-in-memory-web-api',
+      'rxjs':                       'node_modules/rxjs'
+      
+    };
+    // packages tells the System loader how to load when no filename and/or no extension
+    var packages = {
+      'app':                        { main: 'main.js',  defaultExtension: 'js' },
+      'rxjs':                       { defaultExtension: 'js' },
+      'angular2-in-memory-web-api': { main: 'index.js', defaultExtension: 'js' },
+    };
+    var ngPackageNames = [
+      'common',
+      'compiler',
+      'core',  
+      'animations' ,  
+      'material',         
+      'forms',
+      'http',
+      'platform-browser',
+      'platform-browser-dynamic',
+      'router',
+      'router-deprecated',
+      'upgrade',
+    ];
+    // Individual files (~300 requests):
+    function packIndex(pkgName) {
+      packages['@angular/'+pkgName] = { main: 'index.js', defaultExtension: 'js' };
+    }
+    // Bundled (~40 requests):
+    function packUmd(pkgName) {
+      packages['@angular/'+pkgName] = { main: 'bundles/' + pkgName + '.umd.js', defaultExtension: 'js' };
+    }
+    // Most environments should use UMD; some (Karma) need the individual index files
+    var setPackageConfig = System.packageWithIndex ? packIndex : packUmd;
+    // Add package entries for angular packages
+    ngPackageNames.forEach(setPackageConfig);
+    var config = {
+      map: map,
+      packages: packages
+    };
+    System.config(config);
+  })(this);
