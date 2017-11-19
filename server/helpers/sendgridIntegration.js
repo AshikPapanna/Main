ae382fdd21b0826d87f773d775d34f417dd106d5
Ashik Papanna <Ashikvpapanna@gmail.com>
2017-11-19 23:03:27 +0530

    Added Notification template

--- server/helpers/sendgridIntegration.js
+++ server/helpers/sendgridIntegration.js
@@ -4,7 +4,7 @@ var fromEmail = new helper.Email('ashikvpapanna@gmail.com','Team Sarang');
 var toEmail = new helper.Email('ashikvp1993@gmail.com');
 var subject = 'Sending with SendGrid is Fun';
 var content = new helper.Content('text/html', '<h1>and easy to do anywhere, even with Node.js</h1>'+
-'<div style="background-color:red;color:white;"><p style="padding:2%;font-size: 200%;font-family: ‘Merriweather’, arial, sans-serif;">S&#195;RANG</p></div>');
+'<div style="background: linear-gradient(to right, red , blue);;color:white;"><p style="padding:2%;font-size: 200%;font-family: ‘Merriweather’, arial, sans-serif;">S&#195;RANG</p></div>');
 var mail = new helper.Mail(fromEmail, subject, toEmail, content);
  
 var sg = require('sendgrid')(appconfig.sendgridkey);
