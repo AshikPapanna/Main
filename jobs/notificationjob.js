5aafeacb5bd7cd575cf089713daec8398d2ef07e
Ashik Papanna <Ashikvpapanna@gmail.com>
2017-11-19 23:34:55 +0530

    Added Notification template

--- jobs/notificationjob.js
+++ jobs/notificationjob.js
@@ -6,7 +6,7 @@ var notificationmailhelper=require('../server/helpers/notificationsendgrid.js');
 var User=require('../schemas/user.js'); 
  
 exports.sendnotification=function(req,res,next){
-schedule.scheduleJob('*/1 * * * *', function(){
+schedule.scheduleJob('0 6 * * *', function(){
   User.find({role:'Admin'},function(err,users){
     if(err){
       console.log(err);
