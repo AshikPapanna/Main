96a53ef58de1dbda9c80a56b8cfe02a70ed45980
Ashik P <ashikvpapanna@gmail.com>
2018-01-05 16:47:33 +0530

    added filr read

--- server/helpers/sendgridIntegration.js
+++ server/helpers/sendgridIntegration.js
@@ -1,18 +1,31 @@
 var helper = require('sendgrid').mail;
+var fs = require('fs');
+
+var bitmap = fs.readFileSync('../../public/images/gravatar.jpg');
+// convert binary data to base64 encoded string
+var a= new Buffer(bitmap).toString('base64');
+
 var appconfig=require('../../appconfig');
 var fromEmail = new helper.Email('ashikvpapanna@gmail.com','Team Sarang');
 var toEmail = new helper.Email('ashikvp1993@gmail.com');
+var attachement=new helper.Attachment();
+attachement.setContent(a);
+attachement.setContentId('hd1');
+attachement.setFilename('gravatar.jpg')
+attachement.setType('image/jpg');
+
 var subject = 'Sending with SendGrid is Fun';
 var content = new helper.Content('text/html', '<h1>and easy to do anywhere, even with Node.js</h1>'+
-'<style>@import url(https://fonts.googleapis.com/css?family=Comfortaa|Exo+2|Kanit|Righteous)</style>'+
-'<div style="background-color:#581845;padding:0.25%;padding-left: 2%;"> <h1 style="font-family: Verdana, Arial, Helvetica, sans-serif;"><span style="color:white;">S&#x0101;rang</span></h1>');
+'<img alt="dsfdf" src="data:image/jpg;base64,'+a+'"/>');
 var mail = new helper.Mail(fromEmail, subject, toEmail, content);
+mail.addAttachment(attachement);
+
  
 var sg = require('sendgrid')(appconfig.sendgridkey);
 var request = sg.emptyRequest({
   method: 'POST',
   path: '/v3/mail/send',  
-  body: mail.toJSON()
+  body: mail.toJSON(),  
 });
  
 sg.API(request, function (error, response) {
